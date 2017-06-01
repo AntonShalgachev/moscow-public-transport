5bf8dd7725f176b9f2f5ec4e2935ec14c5c5afff
Anton Shalgachev <anton.shalgachev@gmail.com>
2017-06-01 09:21:22 +0300

    'add transport' view

--- app/src/main/java/com/shalgachev/moscowpublictransport/activities/AddTransportActivity.java
+++ app/src/main/java/com/shalgachev/moscowpublictransport/activities/AddTransportActivity.java
@@ -2,14 +2,17 @@ package com.shalgachev.moscowpublictransport.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 
 import com.shalgachev.moscowpublictransport.R;
+import com.shalgachev.moscowpublictransport.adapters.MyAdapter;
 import com.shalgachev.moscowpublictransport.data.TransportType;
 
 public class AddTransportActivity extends AppCompatActivity {
-    private TransportType mTransportType;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,35 +24,65 @@ public class AddTransportActivity extends AppCompatActivity {
         }
 
         mTransportType = (TransportType) getIntent().getExtras().getSerializable("transport_type");
-
         if (mTransportType == null)
             throw new IllegalArgumentException("Transport type is null");
 
-        CharSequence extra;
+        TransportData transportData = getTransportData();
+
+        setTitle(getString(R.string.add_transport_title, getString(transportData.titleExtraResource)));
+
+        ImageView transportIcon = (ImageView) findViewById(R.id.image_transport_icon);
+        transportIcon.setBackgroundResource(transportData.iconBackgroundResource);
+        transportIcon.setImageResource(transportData.iconImageResource);
+
+        RecyclerView stopsList = (RecyclerView) findViewById(R.id.list_stops);
+        String[] stops = {"Планерная", "2Б", "Юрма", "Планерная", "2Б", "Юрма", "Планерная", "2Б", "Юрма", "Планерная", "2Б", "Юрма", "Планерная", "2Б", "Юрма"};
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        MyAdapter adapter = new MyAdapter(stops);
+        stopsList.setLayoutManager(layoutManager);
+        stopsList.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home)
+            finish();
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private TransportData getTransportData()
+    {
+        TransportData transportData = new TransportData();
         switch (mTransportType) {
             case BUS:
-                extra = getString(R.string.add_bus_title_extra);
+                transportData.titleExtraResource = R.string.add_bus_title_extra;
+                transportData.iconBackgroundResource = R.drawable.bus_circle;
+                transportData.iconImageResource = R.drawable.bus;
                 break;
             case TROLLEY:
-                extra = getString(R.string.add_trolley_title_extra);
+                transportData.titleExtraResource = R.string.add_trolley_title_extra;
+                transportData.iconBackgroundResource = R.drawable.trolley_circle;
+                transportData.iconImageResource = R.drawable.trolley;
                 break;
             case TRAM:
-                extra = getString(R.string.add_tram_title_extra);
+                transportData.titleExtraResource = R.string.add_tram_title_extra;
+                transportData.iconBackgroundResource = R.drawable.tram_circle;
+                transportData.iconImageResource = R.drawable.tram;
                 break;
             default:
                 throw new IllegalArgumentException("Unexpected transport type");
         }
 
-        setTitle(getString(R.string.add_transport_title, extra));
+        return transportData;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) // Press Back Icon
-        {
-            finish();
-        }
-
-        return super.onOptionsItemSelected(item);
+    private class TransportData
+    {
+        private int titleExtraResource;
+        private int iconBackgroundResource;
+        private int iconImageResource;
     }
+
+    private TransportType mTransportType;
 }
